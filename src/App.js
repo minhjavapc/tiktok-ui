c37effa995b0bf75489f2669218ce5c042aa3020
minhjavapc <104562096+Mingbae0502@users.noreply.github.com>
2022-07-04 14:44:10 +0700

    switch yarn to npm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button';
-
 function App() {
   return (
     <div className="App">
-      <Button />
+      <button>Click me!</button>
     </div>
   );
 }
