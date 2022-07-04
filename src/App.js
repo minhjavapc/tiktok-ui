68823087a770e2d66e646808ce98077a5cf4d682
minhjavapc <104562096+Mingbae0502@users.noreply.github.com>
2022-07-04 14:02:07 +0700

    install prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button />
     </div>
   );
 }
